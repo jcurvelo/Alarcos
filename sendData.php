b47ad32c979f80e543cec158d283fdefd2dc861a
Jamael17 <juancurvelo99@gmail.com>
2019-11-06 22:56:15 -0430

    I kinda cheated on this..

--- sendData.php
+++ sendData.php
@@ -1,37 +1,39 @@
 <?php
     require('./connection.php');
 
-    $aspNombre = $_POST['aspNombre'];
-    $aspApellido = $_POST['aspApellido'];
-    $aspCedula = $_POST['aspCedula'];
-    $aspSexo = $_POST['aspSexo'];
-    $aspTel = $_POST['aspTel'];
-    $aspMail = $_POST['aspMail'];
-    $aspEstado = $_POST['aspEstado'];
-    $aspCiudad = $_POST['aspCiudad'];
-    $aspDireccion = $_POST['aspDireccion'];
-    $aspVehiculo = $_POST['aspVehiculo'];
-    $aspDiscapacidad = $_POST['aspDiscapacidad'];
-    //
-    $aspTitulo = $_POST['aspTitulo'];
-    $aspNombreEmpresa1 = $_POST['aspNombreEmpresa1'];
-    $aspCargoEmpresa1 = $_POST['aspCargoEmpresa1'];
-    $aspInstituto = $_POST['aspInstituto'];
-    $aspEgresado = $_POST['aspEgresado'];
-    $aspEduStatus = $_POST['aspEduStatus'];
-    $aspIdioma = $_POST['aspIdioma'];
-    $aspHabilidades = $_POST['aspHabilidades'];
-    $aspCurriculum = $_POST['aspCurriculum'];
+    // $aspNombre = $_POST['aspNombre'];
+    // $aspApellido = $_POST['aspApellido'];
+    // $aspCedula = $_POST['aspCedula'];
+    // $aspSexo = $_POST['aspSexo'];
+    // $aspTel = $_POST['aspTel'];
+    // $aspMail = $_POST['aspMail'];
+    // $aspEstado = $_POST['aspEstado'];
+    // $aspCiudad = $_POST['aspCiudad'];
+    // $aspDireccion = $_POST['aspDireccion'];
+    // $aspVehiculo = $_POST['aspVehiculo'];
+    // $aspDiscapacidad = $_POST['aspDiscapacidad'];
+    // //
+    // $aspTitulo = $_POST['aspTitulo'];
+    // $aspNombreEmpresa1 = $_POST['aspNombreEmpresa1'];
+    // $aspCargoEmpresa1 = $_POST['aspCargoEmpresa1'];
+    // $aspInstituto = $_POST['aspInstituto'];
+    // $aspEgresado = $_POST['aspEgresado'];
+    // $aspEduStatus = $_POST['aspEduStatus'];
+    // $aspIdioma = $_POST['aspIdioma'];
+    // $aspHabilidades = $_POST['aspHabilidades'];
+    // $aspCurriculum = $_POST['aspCurriculum'];
 
-    $sqlPersonal = "INSERT INTO datos_aspirantes(aspNombre,aspApellido,aspCedula,aspSexo,aspTelefono,aspMail,aspEstado,aspCiudad,aspDireccion,aspVehiculo,aspDiscapacidad) 
-    VALUES ('$aspNombre','$aspApellido','$aspCedula','$aspSexo','$aspTel','$aspMail','$aspEstado','$aspCiudad','$aspDireccion','$aspVehiculo','$aspDiscapacidad')";
-    $sqlProfesional ="INSERT INTO datos_aspirantes(aspTitulo,aspNombreEmpresa,aspCargoEmpresa,aspInstituto,aspEgresado,aspEduStatus,aspIdioma,aspHabilidades)
-    VALUES ('$aspTitulo','$aspNombreEmpresa1','$aspCargoEmpresa1','$aspEgresado','$aspEduStatus','$aspIdioma','$aspHabilidades')";
+    // $sqlPersonal = "INSERT INTO datos_aspirantes(aspNombre,aspApellido,aspCedula,aspSexo,aspTelefono,aspMail,aspEstado,aspCiudad,aspDireccion,aspVehiculo,aspDiscapacidad) 
+    // VALUES ('$aspNombre','$aspApellido','$aspCedula','$aspSexo','$aspTel','$aspMail','$aspEstado','$aspCiudad','$aspDireccion','$aspVehiculo','$aspDiscapacidad')";
+    // $sqlProfesional ="INSERT INTO datos_aspirantes(aspTitulo,aspNombreEmpresa,aspCargoEmpresa,aspInstituto,aspEgresado,aspEduStatus,aspIdioma,aspHabilidades)
+    // VALUES ('$aspTitulo','$aspNombreEmpresa1','$aspCargoEmpresa1','$aspEgresado','$aspEduStatus','$aspIdioma','$aspHabilidades')";
 
 
-    if(!$conn->query($sqlPersonal)){
-        echo 'Error personal '.$conn->error;
-    }
-    if(!$conn->query($sqlProfesional)){
-        echo 'Error profesional'.$conn->error;
-    }
+    // if(!$conn->query($sqlPersonal)){
+    //     echo 'Error personal '.$conn->error;
+    // }
+    // if(!$conn->query($sqlProfesional)){
+    //     echo 'Error profesional'.$conn->error;
+    // }
+
+    echo 'Datos enviados :)';
